cbadd28687eb67fdb2825545fb42f38769f07fd4
Aprendible <jorge@aprendible.com>
2023-08-16 17:21:29 -0500

    06 - Rutas en Laravel 10

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/','welcome')->name('welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/chirps', function () {
+    return 'Welcome to our chirps page';
+})->name('chirps.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
