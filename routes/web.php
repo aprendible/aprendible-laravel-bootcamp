bed08b9b201da82becd1f00d662d088eefccba63
Aprendible <jorge@aprendible.com>
2023-08-17 20:46:09 -0500

    07 - Middleware en Laravel 10

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome')->name('welcome');
 
-Route::get('/chirps', function () {
-    return 'Welcome to our chirps page';
-})->name('chirps.index');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', function () {
+        return 'Welcome to our chirps page';
+    })->name('chirps.index');
 });
 
 require __DIR__.'/auth.php';
